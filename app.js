dd9a0727b5fe4e13ff6d95e6484551ef4a3cfccc
asawarik@andrew.cmu.edu <asawarik@andrew.cmu.edu>
2017-12-07 17:23:27 -0500

    re commented the working code

--- app.js
+++ app.js
@@ -8,40 +8,40 @@ var client = new Cronofy({
 var morgan = require('morgan')
 var express = require("express");
 var app = express()
-// var request = require('request');
+var request = require('request');
 
-// // Set the headers
-// var headers = {
-//     'Accept':     'application/json',
-//     'Accept-Charset': 'utf-8'
-// }
+// Set the headers
+var headers = {
+    'Accept':     'application/json',
+    'Accept-Charset': 'utf-8'
+}
 
-// // Configure the request
-// var options = {
-//     headers: headers,
-//     uri: 'https://api.cronofy.com/oauth/token',
-//     method: 'POST',
-//     json: { "client_id": "kr6-FkR4BVLCYE2uQHbzqPSGI_6rWV-D",
-//             "client_secret": "jr-6gY6mQAgOa3KhbLhIYP0F6WjpRBMKXUPNrFB5WGwllrNM2Ao6PPQHtOb2m0zyDUH4D_-K2RFfOcy--ML8wA",
-//             "grant_type": "authorization_code",
-//             "code":"3xkDicUpyONtKVVvzI6nrzI_G3kZmKMs",
-//             "redirect_uri": "http://localhost:50000/"
-//         }   
-// };
+// Configure the request
+var options = {
+    headers: headers,
+    uri: 'https://api.cronofy.com/oauth/token',
+    method: 'POST',
+    json: { "client_id": "kr6-FkR4BVLCYE2uQHbzqPSGI_6rWV-D",
+            "client_secret": "jr-6gY6mQAgOa3KhbLhIYP0F6WjpRBMKXUPNrFB5WGwllrNM2Ao6PPQHtOb2m0zyDUH4D_-K2RFfOcy--ML8wA",
+            "grant_type": "authorization_code",
+            "code":"3xkDicUpyONtKVVvzI6nrzI_G3kZmKMs",
+            "redirect_uri": "http://localhost:50000/"
+        }   
+};
 
-// var morgan = require('morgan')
-// var express = require("express");
-// var app = express()
-// app.get("/test2", function(req, res) {
-// request(options, function (error, response, body) {
-//     //console.log(options);
-//     console.log("error!");
-//     console.log(body);
-//   if (!error && response.statusCode == 200) {
-//     console.log(body.id) // Print the shortened url.
-//   }
-// });
-// });
+var morgan = require('morgan')
+var express = require("express");
+var app = express()
+app.get("/test2", function(req, res) {
+request(options, function (error, response, body) {
+    //console.log(options);
+    console.log("error!");
+    console.log(body);
+  if (!error && response.statusCode == 200) {
+    console.log(body.id) // Print the shortened url.
+  }
+});
+});
 
 var options1 = {
   tzid: 'Etc/UTC'
@@ -53,14 +53,14 @@ var options2 = {
   tzid: 'Etc/UTC'
 };
 
-client.listCalendars(options1)
-  .then(function (response) {
-        console.log("LIST CALANDERS");
-      var calendars = response.calendars;
-      console.log(calendars);
-  });
-//var http = require("http");
-// var request = require("request");
+// client.listCalendars(options1)
+//   .then(function (response) {
+//         console.log("LIST CALANDERS");
+//       var calendars = response.calendars;
+//       console.log(calendars);
+//   });
+var http = require("http");
+var request = require("request");
 
 
 // var express = require("express");
